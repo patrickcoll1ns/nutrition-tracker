bbb5dab32fd74492061e6a3deb789529c42efa6b
Patrick Collins <patrickcoll1ns@icloud.com>
2026-07-07 21:29:07 -0400

    Building a hardcoded single food entry

--- project.py
+++ project.py
@@ -1,1 +1,9 @@
-dict = {"date": entry, "food": entry, }
+date = "7/7/2026"
+food = "chicken breast"
+calories = 165
+protein = 30
+carbs = 0
+fat = 3
+entry = {"date": date, "food": food, "calories": calories, "protein": protein,
+         "carbs": carbs, "fat": fat}
+print(entry)
