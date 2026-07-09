c5c2b704e55e5752fd7f1610f77d4b32afdbcc7e
Patrick Collins <patrickcoll1ns@icloud.com>
2026-07-08 22:56:06 -0400

    Allow the user to give inputs
    the user can now input their own foods and calories now and can break the questionnaire with control d

--- project.py
+++ project.py
@@ -1,29 +1,21 @@
-date = "7/7/2026"
-food = "chicken breast"
-calories = 165
-protein = 30
-carbs = 0
-fat = 3
-entry0 = {"date": date, "food": food, "calories": calories, "protein": protein,
-         "carbs": carbs, "fat": fat}
-date = "7/7/2026"
-food = "rice"
-calories = 205
-protein = 4.3
-carbs = 44.5
-fat = 0.4
-entry1 = {"date": date, "food": food, "calories": calories, "protein": protein,
-         "carbs": carbs, "fat": fat}
-date = "7/7/2026"
-food = "broccoli"
-calories = 90
-protein = 2.5
-carbs = 6
-fat = 0.3
-entry2 = {"date": date, "food": food, "calories": calories, "protein": protein,
-         "carbs": carbs, "fat": fat}
-entries = [entry0, entry1, entry2]
-total = 0
-for entry in entries:
-    total += entry["calories"]
-print(total)
+def main():
+    entries = []
+    date = input("What is the date today? ")
+    while True: 
+        try: 
+            food = input("What food did you eat? ")
+            calories = int(input("How many calories did it have? "))
+            protein = float(input("How many grams of protein did it have? "))
+            carbs = float(input("How many grams of carbs did it have? "))
+            fat = float(input("How many grams of fat did it have? "))
+            entry = make_entry(date, food, calories, protein, carbs, fat)
+            entries.append(entry)
+        except EOFError:
+            break
+    print(entries)
+
+def make_entry(date, food, calories, protein, carbs, fat):
+    return {"date": date, "food": food, "calories": calories, "protein": protein,
+            "carbs": carbs, "fat": fat}
+
+main()
