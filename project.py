58434c2b0ce57fb5942943712c0c564b733defc7
Patrick Collins <patrickcoll1ns@icloud.com>
2026-07-10 22:45:55 -0400

    load and save function for the json
    I need to have a function to load the file of entries before the user creates one and have it save after every one is made

--- project.py
+++ project.py
@@ -29,6 +29,18 @@ def make_entry(date, food, calories, protein, carbs, fat):
     return {"date": date, "food": food, "calories": calories, "protein": protein,
             "carbs": carbs, "fat": fat}
 
+def load(f):
+    try:
+        with open(f) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+    
+def save(path, data):
+    with open(path, "w") as file:
+        json.dump(data, file)
+
+
 
 if __name__ == "__main__":
     main()
