350967cc951973e6158d1656a75b0cb818fb879b
Patrick Collins <patrickcoll1ns@icloud.com>
2026-07-12 23:04:24 -0400

    testing load and save functions
    Making sure save and load round-trip behavior works properly, using pytest tmp_path fixture

--- project.py
+++ project.py
@@ -30,10 +30,10 @@ def make_entry(date, food, calories, protein, carbs, fat):
     return {"date": date, "food": food, "calories": calories, "protein": protein,
             "carbs": carbs, "fat": fat}
 
-def load(f):
+def load(path):
     try:
-        with open(f) as f:
-            return json.load(f)
+        with open(path) as file:
+            return json.load(file)
     except FileNotFoundError:
         return []
     
---
+++ test_load_and_save.py
@@ -0,0 +1,22 @@
+from project import load, save
+
+entry = [{"date": "7/11/26", 
+                "food": "chicken", 
+                "calories": 190, 
+                "protein": 30.0, 
+                "carbs": 4.5, 
+                "fat": 0.3}, 
+            ]
+
+
+def test_load_and_save(tmp_path):
+    file_path = tmp_path / "entry.json"
+    save(file_path, entry)
+    loaded = load(file_path)
+    
+    assert loaded == entry
+
+
+def test_fileNotFound(tmp_path):
+    file_path = tmp_path / "missing.json"
+    assert load(file_path) == []
