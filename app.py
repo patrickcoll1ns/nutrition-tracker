72637b38a0def4f5dcc5271d71697a95c3823c13
Patrick Collins <patrickcoll1ns@icloud.com>
2026-07-15 22:03:58 -0400

    Saving each entry without a rerun getting rid of the previous entry

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from project import total, make_entry, load, save
 
 st.title("nutrition tracker")
 st.write("Log foods")
+
+if "entries" not in st.session_state:
+    st.session_state["entries"] = []
